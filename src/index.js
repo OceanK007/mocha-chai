fda51c8b628f6ab431e1ad3c0e02f337d92d0504
Kanhaiya Thakur <kanthaku1@publicisgroupe.net>
2018-08-08 19:14:41 +0530

    First commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// No need to provide, since we already provided 'webpack.ProvidePlugin'
+// import '../node_modules/jquery/dist/jquery'
+
+import '../node_modules/jquery-ui-bundle/jquery-ui';
+import './js/greeter'; // OR //const Greeter = require('./js/greeter.js');
+
+import './scss/style.scss';
